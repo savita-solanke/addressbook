6a807194f70c8fe43b6f45b4496dac20ca9defe6
amitkumar2283 <amitkumar2283@gmail.com>
2019-08-14 17:33:57 +0530

    Update ContactService.java

--- addressbook_main/src/main/java/com/edurekademo/tutorial/addressbook/backend/ContactService.java
+++ addressbook_main/src/main/java/com/edurekademo/tutorial/addressbook/backend/ContactService.java
@@ -16,7 +16,7 @@ import java.util.logging.Logger;
 public class ContactService {
 
     // Create dummy data by randomly combining first and last names
-    static String[] fnames = { "Peter", "Alice", "John", "Mike", "Olivia",
+    static String[] fnames = { "Amit", "Edureka", "AnotherAmit", "Mike", "Olivia",
             "Nina", "Alex", "Rita", "Dan", "Umberto", "Henrik", "Rene", "Lisa",
             "Linda", "Timothy", "Daniel", "Brian", "George", "Scott",
             "Jennifer" };
